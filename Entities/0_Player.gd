70519d4b3759f33888b4f9d7b2a65cf39beddee2
Rackmanyloafs <Rackmanyloafs@tuta.io>
2020-07-10 19:49:32 -0400

    can move ladybug

--- Entities/0_Player.gd
+++ Entities/0_Player.gd
@@ -2,27 +2,27 @@ extends Area2D
 
 var TILE_WIDTH = 24
 
-var AMOUNT_OF_FRAMES_PER_MOVEMENT = 3
-var current_lerp_val = 0
+var AMOUNT_OF_FRAMES_PER_MOVEMENT = 3.0
+var current_lerp_val = float(0)
 var moving = false
 var lerp_destination = Vector2.ZERO
 
 func _input(event):
 	if !moving:
 		if event.is_action_pressed("ui_right"):
-			start_moving(position + Vector2.RIGHT)
+			start_moving(Vector2.RIGHT)
 		if event.is_action_pressed("ui_left"):
-			start_moving(position + Vector2.LEFT)
+			start_moving(Vector2.LEFT)
 		if event.is_action_pressed("ui_up"):
-			start_moving(position + Vector2.UP)
+			start_moving(Vector2.UP)
 		if event.is_action_pressed("ui_down"):
-			start_moving(position + Vector2.DOWN)
+			start_moving(Vector2.DOWN)
 		
 		
 func start_moving(direction:Vector2):
 	lerp_destination = position + direction*self.TILE_WIDTH
 	moving = true
-	current_lerp_val = 0
+	current_lerp_val = float(0)
 	move()
 
 func move():
