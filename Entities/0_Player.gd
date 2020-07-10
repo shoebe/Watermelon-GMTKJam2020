2c5b5732e3552ccdf10194bfbe04f81d51eed909
Rackmanyloafs <Rackmanyloafs@tuta.io>
2020-07-10 19:11:31 -0400

    added the start of a player

--- Entities/0_Player.gd
+++ Entities/0_Player.gd
@@ -1,16 +1,27 @@
-extends Node
+extends Area2D
 
+var curent_lerp_val = 0
+var moving = false
+var lerp_destination = Vector2.ZERO
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _input(event):
+	if event.is_action_pressed("ui_right"):
+		lerp_destination = position + Vector2.RIGHT
+		move(position + Vector2.RIGHT)
+	if event.is_action_pressed("ui_left"):
+		lerp_destination = position + Vector2.LEFT
+		move(position + Vector2.LEFT)
+	if event.is_action_pressed("ui_up"):
+		lerp_destination = position + Vector2.UP
+		move(position + Vector2.UP)
+	if event.is_action_pressed("ui_down"):
+		lerp_destination = position + Vector2.DOWN
+		move(position + Vector2.DOWN)
+		
+		
+func move(direction:Vector2):
+	
+	
+func _physics_process(delta):
+	if moving:
+		moving = move(cur_moving_dir)
