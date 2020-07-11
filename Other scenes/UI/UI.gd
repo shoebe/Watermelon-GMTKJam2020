e85fab6d2650967d9572a0aa4407d75fbb507170
Rackmanyloafs <Rackmanyloafs@tuta.io>
2020-07-11 14:34:15 -0400

    initial ui stuff

--- Other scenes/UI/UI.gd
+++ Other scenes/UI/UI.gd
@@ -7,7 +7,11 @@ func loadData():
 	var levelData = get_node("/root/LevelData").get_level_data()
 	max_move_count = levelData["total_moves"]
 	move_count = 0
+	create_arrows()
 	decrement_counter()
+
+func create_arrows():
+	pass
 
 func decrement_counter():
 	$Label.text = String(max_move_count - move_count)
