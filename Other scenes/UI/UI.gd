9b400d2e5a52477d876ad91f1526b71da90bfab3
Rackmanyloafs <Rackmanyloafs@tuta.io>
2020-07-11 18:27:24 -0400

    done with ui for now

--- Other scenes/UI/UI.gd
+++ Other scenes/UI/UI.gd
@@ -18,21 +18,24 @@ func free_grid():
 
 func create_arrows(forced_moves):
 	var node = $ArrowContainer
+	$ContainerTitle.visible = true
 	for key in forced_moves.keys():
 		var grid_item = load("res://Other scenes/UI/Arrow.tscn")
 		var child = grid_item.instance()
 		node.add_child(child)
 		child.set_dir(forced_moves[key])
-		child.count = key+1
+		child.count = key
 		child.update_text()
 		
 
 func decrement_counter():
-	$Label.text = "Moves left: %s"%(max_move_count - move_count)
+	$Label.text = "%s"%(max_move_count - move_count)
 	var node = $ArrowContainer
 	for child in node.get_children():
 		child.count -= 1
 		child.update_text()
-		if child.count <= 0:
+		if child.count < 0:
 			child.free()
+	if node.get_child_count() == 0:
+		$ContainerTitle.visible = false
 	move_count += 1
