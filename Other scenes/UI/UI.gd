21ffde4dde409480ab37a36a16cf8a2640471e75
Rackmanyloafs <Rackmanyloafs@tuta.io>
2020-07-12 10:52:51 -0400

    added box moves to UI

--- Other scenes/UI/UI.gd
+++ Other scenes/UI/UI.gd
@@ -10,20 +10,36 @@ func loadData():
 	max_move_count = levelData["total_moves"]
 	move_count = 0
 	var forced_moves = levelData["forced_moves"]
-	create_arrows(forced_moves)
+	var box_moves
+	if "box_moves" in levelData.keys():
+		box_moves = levelData["box_moves"]
+	else:
+		box_moves = {}
+	create_arrows(forced_moves, box_moves)
 	decrement_counter()
 
 func free_grid():
 	for child in $ArrowContainer.get_children():
 		child.free()
+	for child in $BoxArrowContainer.get_children():
+		child.free()
 
-func create_arrows(forced_moves):
+func create_arrows(forced_moves, box_moves):
 	var node = $ArrowContainer
 	$ContainerTitle.visible = true
 	for key in forced_moves.keys():
 		var child = grid_item.instance()
 		node.add_child(child)
 		child.set_dir(forced_moves[key])
+		child.count = key
+		child.update_text()
+	
+	$ContainerTitle2.visible = true
+	node = $BoxArrowContainer
+	for key in box_moves.keys():
+		var child = grid_item.instance()
+		node.add_child(child)
+		child.set_dir(box_moves[key])
 		child.count = key
 		child.update_text()
 		
@@ -38,4 +54,13 @@ func decrement_counter():
 			child.free()
 	if node.get_child_count() == 0:
 		$ContainerTitle.visible = false
+		
+	node = $BoxArrowContainer
+	for child in node.get_children():
+		child.count -= 1
+		child.update_text()
+		if child.count < 0:
+			child.free()
+	if node.get_child_count() == 0:
+		$ContainerTitle2.visible = false
 	move_count += 1
